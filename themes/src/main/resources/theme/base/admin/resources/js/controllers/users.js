fdaee144d08c4fc937b550e9945f7dbe206a9911
Christophe Frattino <christophe.frattino@elca.ch>
2019-10-04 09:20:38 +0200

    CLOUDTRUST-1763 No longer disable the Send email button after sending Credential Reset

--- themes/src/main/resources/theme/base/admin/resources/js/controllers/users.js
+++ themes/src/main/resources/theme/base/admin/resources/js/controllers/users.js
@@ -699,7 +699,6 @@ module.controller('UserCredentialsCtrl', function($scope, realm, user, $route, R
         Dialog.confirm('Send Email', 'Are you sure you want to send email to user?', function() {
             UserExecuteActionsEmail.update({ realm: realm.realm, userId: user.id, lifespan: $scope.emailActionsTimeout.toSeconds() }, $scope.emailActions, function() {
                 Notifications.success("Email sent to user");
-                $scope.emailActions = [];
             }, function() {
                 Notifications.error("Failed to send email to user");
             });
