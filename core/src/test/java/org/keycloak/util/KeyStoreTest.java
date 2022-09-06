40776a8743b058cc4bab8d72ff2c9ac74e7b5885
mposolda <mposolda@gmail.com>
2022-09-06 19:39:08 +0200

    Foo Bar Baz

--- core/src/test/java/org/keycloak/util/KeyStoreTest.java
+++ core/src/test/java/org/keycloak/util/KeyStoreTest.java
@@ -26,7 +26,7 @@ public abstract class KeyStoreTest {
         logger.infof("Default keystore type: %s", KeyStore.getDefaultType());
         String userDir = System.getProperty("user.dir");
         logger.infof("user.dir: %s", userDir);
-        logger.infof("File exists 1: %s, File exists 2: %s, File exists: 3: %s", new File(userDir).exists(), new File(userDir + "/test-classes"), new File(userDir + "/test-classes/kc.java.security"));
+        logger.infof("File exists 1: %s, File exists 2: %s, File exists: 3: %s", new File(userDir).exists(), new File(userDir + "/test-classes").exists(), new File(userDir + "/test-classes/kc.java.security").exists());
 
         logKeyStore("JKS");
         logKeyStore("PKCS12");
